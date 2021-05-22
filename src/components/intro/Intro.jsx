1ec2096a2d949fc771b6a17c87fb5a5cbaaa87f4
shilpi <shilpi.agrawal191@gmail.com>
2021-05-22 18:55:39 +0530

    I issue

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -19,7 +19,7 @@ export default function Intro() {
         <div className="intro" id="intro">
         <div className="left">
             <div className="imgContainer">
-                <img className="meimg" src="images/mee2.jpeg" alt=""/>
+                <img className="meimg" src="Images/mee2.jpeg" alt="My img"/>
             </div>
         </div>
         <div className="right">
